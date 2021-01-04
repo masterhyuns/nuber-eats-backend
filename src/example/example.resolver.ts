c6a48439277b861f1b1dcf197d4ee463d32b50af
masterhyuns <masterhyuns@naver.com>
2021-01-04 21:50:56 +0900

    nuber eats 1.3 Arguments

--- src/example/example.resolver.ts
+++ src/example/example.resolver.ts
@@ -1,4 +1,4 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Example } from './entities/example.entity';
 
 @Resolver()
@@ -9,9 +9,15 @@ export class ExampleResolver {
     return true;
   }
 
-  @Query((returns) => Example) // Boolean for graphql
-  getExample() {
+  /**
+   * @Args() => 필요한 것은 요청 해야 하는데 그 요청을 받아주는 것
+   * @Args('veganOnly') veganOnly: boolean  괄호안은 파라미터명, 뒤에는 function을 위한 agruments 명
+   * @param veganOnly
+   */
+  @Query((returns) => [Example]) // Boolean for graphql
+  getExamples(@Args('veganOnly') veganOnly: boolean): Example[] {
     // Example for typescript
-    return true;
+    console.log(veganOnly);
+    return [];
   }
 }
