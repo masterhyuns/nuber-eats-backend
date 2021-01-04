e9d842181207ade3ec24d24ed578994aa49ed8b5
masterhyuns <masterhyuns@naver.com>
2021-01-04 22:28:13 +0900

    nuber eats 1.5 Validating ArgsTypes

--- src/example/dtos/create-example.dto.ts
+++ src/example/dtos/create-example.dto.ts
@@ -1,19 +1,25 @@
 // dto -> data transfer object
 import { ArgsType, Field, InputType } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 @ArgsType() // input 하고자 하는 데이터의 input type
 export class CreateExampleDto {
   @Field((type) => String) // for graphql return type
+  @IsString()
+  @Length(5, 10)
   name: string; // for typescript return type
 
   @Field((type) => Boolean) // for graphql return type
   // for graphql null 허용
-  isVegan?: string; // for typescript return type
+  @IsBoolean()
+  isVegan?: boolean; // for typescript return type
   // for typescript null 허용
 
   @Field((type) => String)
+  @IsString()
   address: string;
 
   @Field((type) => String)
+  @IsString()
   ownerName: string;
 }
