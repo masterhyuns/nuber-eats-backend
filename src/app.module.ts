b837e3e9192ecff65c259861d17027b482b31840
masterhyuns <masterhyuns@naver.com>
2021-01-07 12:27:19 +0900

    nuber eats 2.4 ~5 ConfigService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,23 +2,29 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ExampleModule } from './example/example.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ExampleModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'hyuns',
-      password: '1234',
-      database: 'nuber_eats',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       synchronize: true,
       logging: true,
     }),
+    ExampleModule,
   ],
   controllers: [],
   providers: [],
